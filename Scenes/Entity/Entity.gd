c970a0711173bbf97a8959b01b0c5153dc3fc576
Emily Krohn <emilykrohn2@gmail.com>
2024-03-23 19:28:50 -0700

    Work on functions for entity class

--- Scenes/Entity/Entity.gd
+++ Scenes/Entity/Entity.gd
@@ -3,12 +3,21 @@ extends CharacterBody2D
 class_name Entity
 
 @export var entity_stats: EntityStats
+var health: int
 
-func movement():
-	pass
+func _ready():
+	health = entity_stats.max_health
 
-func damage():
-	pass
+func movement(target: Entity, delta: float):
+	velocity = target.global_position * entity_stats.speed * delta
 
-func take_damage():
-	pass
+func damage(target: Entity):
+	target.take_damage(entity_stats.attack_damage)
+
+func take_damage(amount: int):
+	health -= amount
+	if health <= 0:
+		die()
+
+func die():
+	queue_free()
