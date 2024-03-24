0a4d67d02e7ee13785af830f7a8bff4a22c5f165
Emily Krohn <emilykrohn2@gmail.com>
2024-03-23 17:16:46 -0700

    Create resource for player stats

---
+++ Scenes/Player/player.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+class_name Player
+
+@export var player_stats: PlayerStats
+var direction = Vector2.ZERO
+
+func _physics_process(delta):
+	direction = Vector2(Input.get_axis("ui_left", "ui_right"), Input.get_axis("ui_up", "ui_down")).normalized()
+	
+	if direction:
+		velocity = direction * player_stats.speed * delta
+	else:
+		velocity = Vector2.ZERO
+
+	move_and_slide()
---
+++ Scenes/Player/player_stats.gd
@@ -0,0 +1,10 @@
+extends Resource
+
+class_name PlayerStats
+
+@export var max_health: int
+@export var damage: int
+@export var agility: int
+@export var luck: int
+@export var defense: float
+@export var speed: float
