cdb8d15d662cab82a3ee36f239176746ccb22a8c
Emily Krohn <emilykrohn2@gmail.com>
2024-03-24 21:11:24 -0700

    Create player movement function

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -6,11 +6,13 @@ class_name Player
 var direction = Vector2.ZERO
 
 func _physics_process(delta):
+	player_movement(delta)
+	move_and_slide()
+
+func player_movement(delta: float):
 	direction = Vector2(Input.get_axis("ui_left", "ui_right"), Input.get_axis("ui_up", "ui_down")).normalized()
 	
 	if direction:
 		velocity = direction * player_stats.speed * delta
 	else:
 		velocity = Vector2.ZERO
-
-	move_and_slide()
