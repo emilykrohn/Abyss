2e27b17c0bd1f0ae2de16a1ca963c6d272a6e12d
Emily Krohn <emilykrohn2@gmail.com>
2024-03-24 21:20:18 -0700

    Chort moves to player only when in range

--- Scenes/Enemies/Chort/chort.gd
+++ Scenes/Enemies/Chort/chort.gd
@@ -3,6 +3,16 @@ extends Entity
 class_name Chort
 
 @export var player: Player
+var in_range: bool = false
 
 func _physics_process(delta):
-	movement(self, player, delta)
+	if in_range:
+		movement(self, player, delta)
+
+func _on_outer_range_body_entered(body):
+	if body.name == "Player":
+		in_range = true
+
+func _on_outer_range_body_exited(body):
+	if body.name == "Player":
+		in_range = false
