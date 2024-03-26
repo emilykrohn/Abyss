cceaa0de1bff978d557f964037553fa244706796
Emily Krohn <emilykrohn2@gmail.com>
2024-03-25 21:48:13 -0700

    Fix enemy and chort pathfinding

--- Scenes/Enemies/Chort/chort.gd
+++ Scenes/Enemies/Chort/chort.gd
@@ -2,12 +2,16 @@ extends Enemy
 
 class_name Chort
 
+@onready var nav: NavigationAgent2D = $NavigationAgent2D
 @export var player: Player
 
 func _physics_process(delta):
 	if in_range:
 		movement(self, player, delta)
-		
+
+func get_nav():
+	return nav
+
 func _on_outer_range_body_entered(body):
 	if body.name == "Player":
 		in_range = true
--- Scenes/Enemies/Enemy/enemy.gd
+++ Scenes/Enemies/Enemy/enemy.gd
@@ -2,11 +2,15 @@ extends Entity
 
 class_name Enemy
 
-@onready var nav: NavigationAgent2D = $NavigationAgent2D
-
 var in_range: bool = false
 
 func movement(start:Entity, target: Entity, delta: float):
-	nav.target_position = target.global_position
-	direction = (nav.get_next_path_position() - start.global_position).normalized()
+	start.get_nav().target_position = target.global_position
+	if start.get_nav().is_navigation_finished():
+		return
+	direction = (start.get_nav().get_next_path_position() - start.global_position).normalized()
 	start.velocity = direction * entity_stats.speed * delta
+	move_and_slide()
+
+func get_nav():
+	pass
